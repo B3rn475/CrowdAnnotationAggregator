eb59c92fb373bf574225e5f4af329923a599f397
B3rn475 <b3rn4@hotmail.it>
2014-04-19 09:36:06 +0200

    Added missing Annotation function

--- AnnotationsAggregator/src/it/polimi/annotationsaggregator/Annotation.java
+++ AnnotationsAggregator/src/it/polimi/annotationsaggregator/Annotation.java
@@ -37,4 +37,28 @@ public abstract class Annotation<C extends Content, A extends Annotator> {
 		this.content = content;
 		this.annotator = annotator;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((annotator == null) ? 0 : annotator.hashCode());
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Annotation<?,?>))
+			return false;
+		Annotation<?,?> other = (Annotation<?,?>) obj;
+		if (annotator.equals(other.annotator) && content.equals(other.content))
+			return true;
+		return false;
+	}
 }
