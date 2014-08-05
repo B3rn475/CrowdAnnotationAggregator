9261ff008c855cc92cc42e8ac18991ae4142fc03
b3rn475 <b3rn4@hotmail.it>
2014-08-05 11:44:10 +0200

    Added Comments

--- CrowdAnnotationAggregator/src/it/polimi/crowdannotationaggregator/LinearAggregator.java
+++ CrowdAnnotationAggregator/src/it/polimi/crowdannotationaggregator/LinearAggregator.java
@@ -59,14 +59,33 @@ public abstract class LinearAggregator<A extends Annotation<C, ?>, C extends Con
 		sumAllAnnotations();
 	}
 
+	/**
+	 * This callback must call postSumAllAnnotations()
+	 */
 	protected abstract void sumAllAnnotations();
+	/**
+	 * This callback must call postSubtractAnnotation()
+	 * @param aggregatedAnnotation
+	 * @param annotation
+	 * @param weight
+	 */
 	protected abstract void subtractAnnotation(A aggregatedAnnotation, A annotation, double weight);
 
+	/**
+	 * This callback must be called at the end of subtractAnnotation()
+	 * The annotation must have Annotator.NONE as Annotator
+	 * @param annotation
+	 */
 	protected final void postSumAllAnnotations(A aggregatedAnnotation){
 		this.aggregatedAnnotation = aggregatedAnnotation;
 		postInitializingAggregation();
 	}
 	
+	/**
+	 * This callback must be called at the end of subtractAnnotation()
+	 * The annotation must have the same Annotator of the subtracted Annotation
+	 * @param annotation
+	 */
 	protected final void postSubtractAnnotation(A annotation){
 		postAggregate(annotation);
 	}
